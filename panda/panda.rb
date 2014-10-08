a940fb784e4a897ef14d7ebf175e02b98922c2b9
Jared Williams <jaredwilliamsmail@gmail.com>
2014-10-08 12:54:20 -0500

    prehw commit

--- panda/panda.rb
+++ panda/panda.rb
@@ -7,4 +7,8 @@ class Panda
   def climb(that_with_which_is_climbed)
     that_with_which_is_climbed == :tree
   end
+
+  def sleep_percentage
+    10/24.0
+  end
 end
--- panda/panda_test.rb
+++ panda/panda_test.rb
@@ -5,25 +5,34 @@ require './panda'
 class PandaTest < MiniTest::Test
 
   def test_panda_eats_bamboo_pikas_rodent
-    panda = Panda.new
+# panda = Panda.new you don't need this if you put it on the bottom!!!
     %i(bamboo pikas rodents). each do |food|
     assert_equal true, panda.eat(food)
   end
   end
 
   def test_panda_eats_bacon
-    panda = Panda.new
     assert_equal false, panda.eat(:bacon)
   end
 
   def test_panda_climbs_trees
-    panda = Panda.new
     assert_equal true, panda.climb(:tree)
   end
 
   def test_panda_climbs_hills
-    panda = Panda.new
     assert_equal false, panda.climb(:hill)
   end
+
+  def test_pandas_sleep_a_bunch
+    percent = panda.sleep_percentage
+    assert_equal false, (percent >= bunch_threshold)
+  end
+
+  def bunch_threshold
+    16/24.0
+  end
+
+  panda = Panda.new
+
 
 end
